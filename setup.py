3bc2c2bacee88d7070103ad8402a87da10c7fdc9
Macartur Sousa <macartur.sc@gmail.com>
2018-09-25 14:23:43 -0300

    Fix setup.py to able import napps.kytos.of_core

--- setup.py
+++ setup.py
@@ -3,19 +3,34 @@
 Run "python3 setup --help-commands" to list all available commands and their
 descriptions.
 """
-import json
+import os
+import shutil
+import sys
 from abc import abstractmethod
 from pathlib import Path
 from subprocess import call, check_call
 
 from setuptools import Command, setup
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install
 
+if 'bdist_wheel' in sys.argv:
+    raise RuntimeError("This setup.py does not support wheels")
 
-def read_version_from_json():
-    """Read the NApp version from NApp kytos.json file."""
-    file = Path('kytos.json')
-    metadata = json.loads(file.read_text())
-    return metadata['version']
+# Paths setup with virtualenv detection
+if 'VIRTUAL_ENV' in os.environ:
+    BASE_ENV = Path(os.environ['VIRTUAL_ENV'])
+else:
+    BASE_ENV = Path('/')
+
+# Kytos var folder
+VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
+# Path for enabled NApps
+ENABL_PATH = VAR_PATH / 'napps'
+# Path to install NApps
+INSTL_PATH = VAR_PATH / 'napps' / '.installed'
+CURR_DIR = Path('.').resolve()
 
 
 class SimpleCommand(Command):
@@ -71,7 +86,7 @@ class Linter(SimpleCommand):
     def run(self):
         """Run yala."""
         print('Yala is running. It may take several seconds...')
-        check_call('yala *.py tests', shell=True)
+        check_call('yala *.py tests/test_*.py', shell=True)
 
 
 class CITest(SimpleCommand):
@@ -87,8 +102,77 @@ class CITest(SimpleCommand):
         check_call(cmd, shell=True)
 
 
+class InstallMode(install):
+    """Create files in var/lib/kytos."""
+
+    description = 'To install NApps, use kytos-utils. Devs, see "develop".'
+
+    def run(self):
+        """Create of_core as default napps enabled."""
+        print(self.description)
+
+
+class EggInfo(egg_info):
+    """Prepare files to be packed."""
+
+    def run(self):
+        """Build css."""
+        self._install_deps_wheels()
+        super().run()
+
+    @staticmethod
+    def _install_deps_wheels():
+        """Python wheels are much faster (no compiling)."""
+        print('Installing dependencies...')
+        check_call([sys.executable, '-m', 'pip', 'install', '-r',
+                    'requirements/run.in'])
+
+
+class DevelopMode(develop):
+    """Recommended setup for kytos-napps developers.
+
+    Instead of copying the files to the expected directories, a symlink is
+    created on the system aiming the current source code.
+    """
+
+    description = 'install NApps in development mode'
+
+    def run(self):
+        """Install the package in a developer mode."""
+        super().run()
+        if self.uninstall:
+            shutil.rmtree(str(ENABL_PATH), ignore_errors=True)
+        else:
+            self._create_folder_symlinks()
+            self._create_file_symlinks()
+
+    @staticmethod
+    def _create_folder_symlinks():
+        """Symlink to all Kytos NApps folders.
+
+        ./napps/kytos/napp_name will generate a link in
+        var/lib/kytos/napps/.installed/kytos/napp_name.
+        """
+        links = INSTL_PATH / 'kytos'
+        links.mkdir(parents=True, exist_ok=True)
+        code = CURR_DIR
+        src = links / 'of_core'
+        src.symlink_to(code)
+
+        (ENABL_PATH / 'kytos').mkdir(parents=True, exist_ok=True)
+        dst = ENABL_PATH / Path('kytos', 'of_core')
+        dst.symlink_to(src)
+
+    @staticmethod
+    def _create_file_symlinks():
+        """Symlink to required files."""
+        src = ENABL_PATH / '__init__.py'
+        dst = CURR_DIR / 'napps' / '__init__.py'
+        src.symlink_to(dst)
+
+
 setup(name='kytos_of_core',
-      version=read_version_from_json(),
+      version='1.2.0',
       description='Core Napps developed by Kytos Team',
       url='http://github.com/kytos/of_core',
       author='Kytos Team',
@@ -107,7 +191,10 @@ setup(name='kytos_of_core',
           'clean': Cleaner,
           'ci': CITest,
           'coverage': TestCoverage,
+          'develop': DevelopMode,
+          'install': InstallMode,
           'lint': Linter,
+          'egg_info': EggInfo,
       },
       zip_safe=False,
       classifiers=[
