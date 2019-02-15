d2f2d5f57a58d6c6a178334f8521a3633606cfdc
Humberto Diógenes <hdiogenes@gmail.com>
2019-02-15 13:34:12 -0300

    setup.py port from mef_eline (ref #42)
    Enable of_core install to napps folder and make tox
    see it.

--- setup.py
+++ setup.py
@@ -1,21 +1,41 @@
 """Setup script.
 
-Run "python3 setup --help-commands" to list all available commands and their
+Run "python3 setup.py --help-commands" to list all available commands and their
 descriptions.
 """
 import json
+import os
+import shutil
+import sys
 from abc import abstractmethod
 from pathlib import Path
 from subprocess import call, check_call
+from typing import Any, Union
 
 from setuptools import Command, setup
+from setuptools.command.develop import develop
+# from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install
 
+if 'bdist_wheel' in sys.argv:
+    raise RuntimeError("This setup.py does not support wheels")
 
-def read_version_from_json():
-    """Read the NApp version from NApp kytos.json file."""
-    file = Path('kytos.json')
-    metadata = json.loads(file.read_text())
-    return metadata['version']
+# Paths setup with virtualenv detection
+if 'VIRTUAL_ENV' in os.environ:
+    BASE_ENV = Path(os.environ['VIRTUAL_ENV'])
+else:
+    BASE_ENV = Path('/')
+
+# Kytos var folder
+VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
+# Path for enabled NApps
+ENABLED_PATH = VAR_PATH / 'napps'
+# Path to install NApps
+INSTALLED_PATH = VAR_PATH / 'napps' / '.installed'
+CURRENT_DIR = Path('.').resolve()
+
+## NApps enabled by default
+#CORE_NAPPS = ['of_core']
 
 
 class SimpleCommand(Command):
@@ -71,7 +91,7 @@ class Linter(SimpleCommand):
     def run(self):
         """Run yala."""
         print('Yala is running. It may take several seconds...')
-        check_call('yala *.py tests', shell=True)
+        check_call('yala *.py tests/test_*.py', shell=True)
 
 
 class CITest(SimpleCommand):
@@ -87,9 +107,99 @@ class CITest(SimpleCommand):
         check_call(cmd, shell=True)
 
 
+# class KytosInstall:
+#     """Common code for all install types."""
+#
+#     @staticmethod
+#     def enable_core_napps():
+#         """Enable a NAPP by creating a symlink."""
+#         (ENABLED_PATH / 'kytos').mkdir(parents=True, exist_ok=True)
+#         for napp in CORE_NAPPS:
+#             napp_path = Path('kytos', napp)
+#             src = ENABLED_PATH / napp_path
+#             dst = INSTALLED_PATH / napp_path
+#             src.symlink_to(dst)
+
+
+class InstallMode(install):
+    """Create files in var/lib/kytos."""
+
+    description = 'To install NApps, use kytos-utils. Devs, see "develop".'
+
+    def run(self):
+        """Create of_core as default napps enabled."""
+        print(self.description)
+
+
+# class EggInfo(egg_info):
+#     """Prepare files to be packed."""
+#
+#     def run(self):
+#         """Build css."""
+#         self._install_deps_wheels()
+#         super().run()
+#
+#     @staticmethod
+#     def _install_deps_wheels():
+#         """Python wheels are much faster (no compiling)."""
+#         print('Installing dependencies...')
+#         check_call([sys.executable, '-m', 'pip', 'install', '-r',
+#                     'requirements/run.in'])
+
+
+class DevelopMode(develop):
+    """Recommended setup for kytos-napps developers.
+
+    Instead of copying the files to the expected directories, a symlink is
+    created on the system aiming the current source code.
+    """
+
+    description = 'install NApps in development mode'
+
+    def run(self):
+        """Install the package in a developer mode."""
+        super().run()
+        if self.uninstall:
+            shutil.rmtree(str(ENABLED_PATH), ignore_errors=True)
+        else:
+            self._create_folder_symlinks()
+            self._create_file_symlinks()
+            # KytosInstall.enable_core_napps()
+
+    @staticmethod
+    def _create_folder_symlinks():
+        """Symlink to all Kytos NApps folders.
+
+        ./napps/kytos/napp_name will generate a link in
+        var/lib/kytos/napps/.installed/kytos/napp_name.
+        """
+        links = INSTALLED_PATH / 'kytos'
+        links.mkdir(parents=True, exist_ok=True)
+        code = CURRENT_DIR
+        src = links / 'of_core'
+        src.symlink_to(code)
+
+        (ENABLED_PATH / 'kytos').mkdir(parents=True, exist_ok=True)
+        dst = ENABLED_PATH / Path('kytos', 'of_core')
+        dst.symlink_to(src)
+
+    @staticmethod
+    def _create_file_symlinks():
+        """Symlink to required files."""
+        src: Path = ENABLED_PATH / '__init__.py'
+        dst: Path = CURRENT_DIR / 'napps' / '__init__.py'
+        src.symlink_to(dst)
+
+
+def read_version_from_json():
+    """Read the NApp version from NApp kytos.json file."""
+    file = Path('kytos.json')
+    metadata = json.loads(file.read_text())
+    return metadata['version']
+
 setup(name='kytos_of_core',
       version=read_version_from_json(),
-      description='Core Napps developed by Kytos Team',
+      description='Core NApps developed by Kytos Team',
       url='http://github.com/kytos/of_core',
       author='Kytos Team',
       author_email='of-ng-dev@ncc.unesp.br',
@@ -107,7 +217,11 @@ setup(name='kytos_of_core',
           'clean': Cleaner,
           'ci': CITest,
           'coverage': TestCoverage,
+
+          'develop': DevelopMode,
+          'install': InstallMode,
           'lint': Linter,
+          #'egg_info': EggInfo,
       },
       zip_safe=False,
       classifiers=[
