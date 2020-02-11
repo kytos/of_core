88635b5257ceed599a2233b00738d0042c73397d
Daniela Garcia Toranzo <43040493+dgarc330@users.noreply.github.com>
2020-02-11 15:00:33 -0500

    Sorting imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,8 @@
 """Module to test the napp kytos/of_core."""
-import sys
 import os
+import sys
 from pathlib import Path
-​
+
 BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
-​
 NAPPS_DIR = BASE_ENV / 'var/lib/kytos/'
-​
 sys.path.insert(0, str(NAPPS_DIR))
