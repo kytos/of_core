e4ea01fac4679ee61a9dcd6ebb9781c86a264c18
Gleyberson Andrade <gleybersonandrade@gmail.com>
2020-04-02 11:22:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class Linter(SimpleCommand):
     def run(self):
         """Run yala."""
         print('Yala is running. It may take several seconds...')
-        check_call('yala *.py, shell=True)
+        check_call('yala *.py', shell=True)
 
 
 class CITest(SimpleCommand):
