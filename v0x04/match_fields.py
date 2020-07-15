451a4071651290bdd9035d03b72226e595ce331a
keniachang <45548506+keniachang@users.noreply.github.com>
2020-07-15 16:11:46 -0400

    Update other match fields with MAC address

--- v0x04/match_fields.py
+++ v0x04/match_fields.py
@@ -172,11 +172,11 @@ class MatchDLDst(MatchField):
         """Return a pyof OXM TLV instance."""
         if '/' in self.value:
             value, mask = self.value.split('/')
-            if mask.upper() == 'FF:FF:FF:FF:FF:FF':
+            mask = mask.upper()
+            if mask == 'FF:FF:FF:FF:FF:FF':
                 mask = None
                 oxm_hasmask = False
             else:
-                mask = mask.upper()
                 oxm_hasmask = True
         else:
             value = self.value
@@ -452,11 +452,11 @@ class MatchARPSHA(MatchField):
         """Return a pyof OXM TLV instance."""
         if '/' in self.value:
             value, mask = self.value.split('/')
-            if mask.upper() == 'FF:FF:FF:FF:FF:FF':
+            mask = mask.upper()
+            if mask == 'FF:FF:FF:FF:FF:FF':
                 mask = None
                 oxm_hasmask = False
             else:
-                mask = mask.upper()
                 oxm_hasmask = True
         else:
             value = self.value
@@ -494,11 +494,11 @@ class MatchARPTHA(MatchField):
         """Return a pyof OXM TLV instance."""
         if '/' in self.value:
             value, mask = self.value.split('/')
-            if mask.upper() == 'FF:FF:FF:FF:FF:FF':
+            mask = mask.upper()
+            if mask == 'FF:FF:FF:FF:FF:FF':
                 mask = None
                 oxm_hasmask = False
             else:
-                mask = mask.upper()
                 oxm_hasmask = True
         else:
             value = self.value
